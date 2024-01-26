6557b3eca087f854cb0c3977967056d3230ca258
KevinDaniel10 <150805852+KevinDaniel10@users.noreply.github.com>
2024-01-25 23:33:36 -0500

    Add files via upload

---
+++ GestionU/src/From1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class From1 {
+    public JPanel sistemapanel;
+    private JLabel Titulo;
+    private JTable table1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
